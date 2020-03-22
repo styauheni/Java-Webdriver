9a899dd43623e5fb112242147b9c43843464589b
Yauheni Statsenka <yauhenistatsenka@Yauhenis-MacBook-Air.local>
2020-03-21 21:47:55 -0700

    usps calculate price

--- src/test/java/definitions/UspsStepDefs.java
+++ src/test/java/definitions/UspsStepDefs.java
@@ -3,20 +3,25 @@ package definitions;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.assertj.core.api.AbstractStringAssert;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.internal.bytebuddy.implementation.bytecode.Throw;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import support.TestContext;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
 public class UspsStepDefs {
     @When("I go to Lookup ZIP page by address")
     public void iGoToLookupZIPPageByAddress() {
-        getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         getDriver().findElement(By.xpath("//a[@id='mail-ship-width']")).click();
         getDriver().findElement(By.xpath("//a[contains(@href,'ZipLookupAction!')]")).click();
@@ -24,23 +29,84 @@ public class UspsStepDefs {
     }
 
     @And("I fill out {string} street, {string} city, {string} state")
-    public void iFillOutStreetCityState(String street, String city, String state) {
+    public void iFillOutStreetCityState(String street, String city, String state) throws InterruptedException {
         getDriver().findElement(By.xpath("//input[@id='tAddress']")).sendKeys(street);
         getDriver().findElement(By.xpath("//input[@id='tCity']")).sendKeys(city);
-        getDriver().findElement(By.xpath("//select[@id='tState']")).click();
-        if (state.equalsIgnoreCase("CA")) {
-            getDriver().findElement(By.xpath("//select[@id='tState']/option[@value='CA']")).click();
+        // My way, do not use:
+//        getDriver().findElement(By.xpath("//select[@id='tState']")).click();
+//        if (state.equalsIgnoreCase("CA")) {
+//            getDriver().findElement(By.xpath("//select[@id='tState']/option[@value='CA']")).click();
+//
+//        } else {
+//            throw new RuntimeException("State must be CA");
+//        }
+//          end of long stupid attempt
 
-        } else {
-            throw new RuntimeException("State must be CA");
-        }
+        // Dynamic XPAth, good way:
+//        getDriver().findElement(By.xpath("//select[@id='tState']/option[@value='" + state + "']")).click();
+
+        // The Best way:
+      WebElement stateElement = getDriver().findElement(By.xpath("//select[@id='tState']"));
+      new Select(stateElement).selectByValue(state);
+
+
+//        Thread.sleep(5000);
         getDriver().findElement(By.xpath("//a[@id='zip-by-address']")).click();
     }
 
     @Then("I validate {string} zip code exists in the result")
     public void iValidateZipCodeExistsInTheResult(String zip) {
-        String result = getDriver().findElement(By.xpath("//ul[@class='list-group industry-detail']")).getText();
-        Assertions.assertThat(result).contains(zip);
+//        String result = getDriver().findElement(By.xpath("//ul[@class='list-group industry-detail']")).getText();
+
+        // Create Explicit wait:
+//        WebDriverWait wait = new WebDriverWait(getDriver(),5);
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='zipByAddressDiv']//p")));
+//        String result = getDriver().findElement(By.xpath("//div[@id='zipByAddressDiv']")).getText();
+
+        // Another Explicit wait:
+//        WebDriverWait wait = new WebDriverWait(getDriver(),5);
+//        wait.until(driver -> driver.findElement(By.xpath("//div[@id='zipByAddressDiv']")).getText().length()>0);
+//        String result = getDriver().findElement(By.xpath("//div[@id='zipByAddressDiv']")).getText();
+
+        // Best way:
+        WebDriverWait wait = new WebDriverWait(getDriver(), 5);
+        WebElement resultElement = getDriver().findElement(By.xpath("//div[@id='zipByAddressDiv']"));
+        wait.until(driver -> !resultElement.getText().isEmpty());
+        String result = resultElement.getText();
+
+        assertThat(result).contains(zip);
+
+
+    }
+
+    @When("I go to Calculate Price Page")
+    public void iGoToCalculatePricePage() {
+        getDriver().findElement(By.xpath("//a[@id='mail-ship-width']")).click();
+        getDriver().findElement(By.xpath("//a[contains(@href,'calculate')]")).click();
+
+    }
+
+    @And("I select {string} with {string} shape")
+    public void iSelectWithShape(String country, String shape) throws InterruptedException {
+        WebElement countryElement = getDriver().findElement(By.xpath("//select[@id='CountryID']"));
+        new Select(countryElement).selectByVisibleText(country);
+        getDriver().findElement(By.xpath("//input[@value='Postcard']")).click();
+
+    }
+
+    @And("I define {string} quantity")
+    public void iDefineQuantity(String quantity) {
+        getDriver().findElement(By.xpath("//input[@id='quantity-0']")).sendKeys(quantity);
+    }
+
+    @Then("I calculate the price and validate cost is {string}")
+    public void iCalculateThePrceAndValidateCostIs(String cost) {
+        getDriver().findElement(By.xpath("//input[@value='Calculate']")).click();
+//        WebDriverWait wait = new WebDriverWait(getDriver(), 5);
+        WebElement resultElemrnt = getDriver().findElement(By.xpath("//div[@id='cost-0']"));
+//        wait.until(driver -> !resultElemrnt.getText().equals("$0.00"));
+        String result = resultElemrnt.getText();
+        assertThat(result).contains(cost);
 
     }
 }
--- src/test/resources/features/usps.feature
+++ src/test/resources/features/usps.feature
@@ -5,7 +5,14 @@
     Scenario: Validate ZIP code for Portnov Computer School
       Given I go to "usps" page
       When I go to Lookup ZIP page by address
-      And I fill out "4970 El Camino Real" street, "Los Altos" city, "CA" state
-      Then I validate "94022" zip code exists in the result
+      And I fill out "4440 Twain Avenue" street, "San Diego" city, "CA" state
+      Then I validate "92120" zip code exists in the result
 
+    @usps4
+    Scenario: Calculate price
+      Given I go to "usps" page
+      When I go to Calculate Price Page
+      And I select "United Kingdom (United Kingdom of Great Britain and Northern Ireland)" with "Postcard" shape
+      And I define "325" quantity
+      Then I calculate the price and validate cost is "$390.00"
 
